05fd243511407a300f0b8940f7bfab4fcc308a02
Nova-Cake <62521922+Nova-Cake@users.noreply.github.com>
2021-01-10 11:23:03 -0500

    Add files via upload

---
+++ CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float sensitivity = 12f;
+    public CharacterController controller; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Lets you move the camera using WASD keys
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * sensitivity * Time.deltaTime);
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) // When you right click
+        {
+           Ray ray = cam.ScreenPointToRay(Input.mousePosition); // Create a ray from camera to mouseposition
+           RaycastHit hit; 
+
+           if (Physics.Raycast(ray, out hit)) // If it hits something
+           {
+                // Move the agent
+                agent.SetDestination(hit.point);
+           }
+        }
+    }
+}
